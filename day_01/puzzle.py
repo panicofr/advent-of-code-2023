db0fde2a1c0856b0e13f4ce725a84282607bcc23
Francesco Panico <francesco.panico@fiscozen.it>
2023-12-02 12:11:18 +0100

    feat(day-01): add day 1 part 2 puzzle solution

--- day_01/puzzle.py
+++ day_01/puzzle.py
@@ -1,17 +1,30 @@
 from __future__ import annotations
 
-import string
 
-__all__ = ["extract_number_from_line"]
+import string
+import sys
+import re
+
+__all__ = ["extract_number_from_line", "improved_extract_number_from_line", "process_document"]
 
 def extract_number_from_line(line: str) -> int:
-    table = str.maketrans("", "", string.ascii_letters)
-    only_digits = line.translate(table)
+    only_digits = re.findall("\d", line)
     return int(only_digits[0]) * 10 + int(only_digits[-1])
 
-def process_document(document: str) -> int:
-    return sum(extract_number_from_line(line) for line in document.split("\n"))
+
+def improved_extract_number_from_line(line: str) -> int:
+    keys = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    reg_exp = f"(?=(\d|{'|'.join(keys)}))"
+    all_digits = re.findall(reg_exp, line)
+    converted_digits = list(map(lambda i : int(i) if i.isdigit() else keys.index(i) + 1, all_digits))
+    return int(converted_digits[0]) * 10 + int(converted_digits[-1])
+
+
+def process_document(document: str, improved: bool = False) -> int:
+    repl = lambda line : improved_extract_number_from_line(line) if improved else extract_number_from_line(line)
+    return sum(map(repl, document.split("\n")))
 
 
 if __name__ == "__main__":
-    print(process_document(open("input.txt").read()))
+    improved = len(sys.argv) > 1 and sys.argv[1] == "--improved"
+    print(process_document(open("input.txt").read(), improved))
--- day_01/tests.py
+++ day_01/tests.py
@@ -1,6 +1,6 @@
 import unittest
 
-from puzzle import extract_number_from_line, process_document
+from puzzle import extract_number_from_line, improved_extract_number_from_line, process_document
 
 class TestCase(unittest.TestCase):
     def test_extract_number_from_line(self):
@@ -15,6 +15,25 @@ class TestCase(unittest.TestCase):
         line = "treb7uchet"
         self.assertEqual(extract_number_from_line(line), 77)
 
+    def test_improved_extract_number_from_line(self):
+        line = "two1nine"
+        self.assertEqual(improved_extract_number_from_line(line), 29)
+
+    def test_improved_extract_number_from_line_with_overlapping_numbers(self):
+        line = "eighthree"
+        self.assertEqual(improved_extract_number_from_line(line), 83)
+
+    def test_improved_extract_number_from_line_with_digits_only_line(self):
+        line = "716"
+        self.assertEqual(improved_extract_number_from_line(line), 76)
+
     def test_process_document(self):
         document = "\n".join(["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f",  "treb7uchet"])
         self.assertEqual(process_document(document), 142)
+
+    def test_process_document_improved(self):
+        document = (
+            "two1nine\neightwothree\nabcone2threexyz\nxtwone3four\n"
+            "4nineeightseven2\nzoneight234\n7pqrstsixteen"
+        )
+        self.assertEqual(process_document(document, improved=True), 281)
